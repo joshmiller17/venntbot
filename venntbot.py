b52ea36ba49289e325ff47bef8e18a4cd0911602
Josh Aaron Miller <jmiller@colgate.edu>
2021-07-02 15:04:31 -0400

    Add reset command and format lookup

--- venntbot.py
+++ venntbot.py
@@ -155,13 +155,14 @@ async def on_message(message):
         if (message.content == "test"):
             return # TODO
             await do_tests(message)
-        if (message.content == "renew"):
+        if (message.content == "renew" or message.content == "reset"):
             data = '{"login": "%s", "password": "%s"}' % (username, password)
             response = requests.post(SERVER_URL, data=data.encode('utf-8'), verify=False)
             response = json.loads(response.text)
             auth_token = response["auth_token"] # assume success
             client.auth_token = auth_token
             await communication.send(ctx, "Authentication renewed.")
+            return
     if message.content.startswith("/"):
         if "roll" in message.content or "lookup" in message.content: # FIXME
             await client.process_commands(message)
